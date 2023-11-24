a3ed9bbbbbe0ed42d285ac78a02461fc23bcc685
leonardo-jesus <leonardojesus02@outlook.com>
2023-11-24 17:17:33 -0300

    feat(pydoc): add pydoc to the avltree

--- AVLTree.py
+++ AVLTree.py
@@ -1,39 +1,101 @@
+"""
+Este módulo implementa uma árvore AVL para armazenamento e recuperação eficiente de dados.
+
+Classes:
+    AVLNode: Representa um nó em uma árvore AVL.
+    AVLTree: Implementa a estrutura da árvore AVL.
+
+Módulos externos necessários:
+    - json: Manipulação de dados JSON.
+    - timeit: Medição do tempo de execução.
+    - matplotlib.pyplot: Geração de gráficos.
+
+Uso:
+    1. Crie uma instância da classe AVLTree.
+    2. Insira dados na árvore usando o método insert.
+    3. Execute test_avl_tree para testar o desempenho da inserção e gerar um gráfico.
+
+Exemplo:
+    data_size = 1000
+    data = [
+        {
+            "name": f"Influencer_{i}",
+            "age": 25 + i,
+            "Works At": f"Company_{i % 5}"
+        } for i in range(data_size)
+    ]
+
+    test_avl_tree(data)
+"""
+
 import json
 import timeit
 import matplotlib.pyplot as plt
 
 class AVLNode:
     def __init__(self, key, value):
-        """add together two numbers
-            
-        Args:
-            x (int): First number to add
-            y (int): Second number to add
+        """
+        Inicializa um nó AVL com uma chave e um valor associado.
 
-        Returns:
-            int: sum of x and y
+        Parâmetros:
+            key (int): Chave do nó.
+            value: Valor associado à chave.
         """
         self.key = key
         self.value = value
         self.height = 1
         self.left = None
         self.right = None
- 
+
 class AVLTree:
     def __init__(self):
+        """Inicializa uma árvore AVL vazia."""
         self.root = None
 
     def height(self, node):
+        """
+        Retorna a altura de um nó na árvore.
+
+        Parâmetros:
+            node (AVLNode): Nó cuja altura será calculada.
+
+        Retorna:
+            int: Altura do nó.
+        """
         return node.height if node else 0
 
     def update_height(self, node):
+        """
+        Atualiza a altura de um nó com base nas alturas de seus filhos.
+
+        Parâmetros:
+            node (AVLNode): Nó cuja altura será atualizada.
+        """
         if node:
             node.height = 1 + max(self.height(node.left), self.height(node.right))
 
     def balance_factor(self, node):
+        """
+        Calcula o fator de balanceamento de um nó.
+
+        Parâmetros:
+            node (AVLNode): Nó cujo fator de balanceamento será calculado.
+
+        Retorna:
+            int: Fator de balanceamento do nó.
+        """
         return self.height(node.left) - self.height(node.right) if node else 0
 
     def rotate_right(self, y):
+        """
+        Realiza uma rotação para a direita em torno do nó 'y'.
+
+        Parâmetros:
+            y (AVLNode): Nó em torno do qual a rotação será realizada.
+
+        Retorna:
+            AVLNode: Novo nó raiz após a rotação.
+        """
         x = y.left
         T2 = x.right
 
@@ -46,6 +108,15 @@ class AVLTree:
         return x
 
     def rotate_left(self, x):
+        """
+        Realiza uma rotação para a esquerda em torno do nó 'x'.
+
+        Parâmetros:
+            x (AVLNode): Nó em torno do qual a rotação será realizada.
+
+        Retorna:
+            AVLNode: Novo nó raiz após a rotação.
+        """
         y = x.right
         T2 = y.left
 
@@ -58,9 +129,27 @@ class AVLTree:
         return y
 
     def insert(self, key, value):
+        """
+        Insere um novo nó na árvore AVL.
+
+        Parâmetros:
+            key (int): Chave do novo nó.
+            value: Valor associado à chave.
+        """
         self.root = self._insert(self.root, key, value)
 
     def _insert(self, node, key, value):
+        """
+        Função auxiliar para realizar a inserção recursiva de um nó na árvore AVL.
+
+        Parâmetros:
+            node (AVLNode): Nó atual sendo considerado.
+            key (int): Chave do novo nó.
+            value: Valor associado à chave.
+
+        Retorna:
+            AVLNode: Novo nó raiz após a inserção.
+        """
         if node is None:
             return AVLNode(key, value)
 
@@ -93,6 +182,12 @@ class AVLTree:
         return node
 
 def test_avl_tree(data):
+    """
+    Testa o desempenho da inserção de dados em uma árvore AVL e gera um gráfico.
+
+    Parâmetros:
+        data: Dados a serem inseridos na árvore.
+    """
     avl_tree = AVLTree()
 
     def insert_data():
